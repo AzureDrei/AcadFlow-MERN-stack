b30ab7e7f475162dcd990265480f5d4e8ced401b
Rafael Brian <rafaelbutche@gmail.com>
2026-08-06 23:49:43 +0800

    fix: rename image file

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import applogo from "../image/AcadFlow-icon.png";
+import applogo from "../image/AcadFlow-Icon.png";
 
 import "./LoginPage.css";
 
